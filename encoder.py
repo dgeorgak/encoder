5e9ddc6dc3cd8eafa1a5c81667499670288e3266
dgeorgak <dimgeorgak@pm.me>
2024-12-05 22:38:52 +0200

    add ability to decode

--- encoder.py
+++ encoder.py
@@ -3,16 +3,23 @@ import argparse
 
 def encode_base64(s):
     return base64.b64encode(s.encode()).decode()
+def decode_base64(s):
+    return base64.b64decode(s).decode()
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-f')
     parser.add_argument('string')
+    parser.add_argument('-d', action='store_true')
     args = parser.parse_args()
 
     if args.f == 'base64':
-        encoded_string = encode_base64(args.string)
-        print(encoded_string)
+        if args.d:
+            decoded_string = decode_base64(args.string)
+            print(decoded_string)
+        else:
+            encoded_string = encode_base64(args.string)
+            print(encoded_string)
     else:
         print(f"Unsupported format: {args.f}")
 
